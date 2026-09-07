2ab6f0bc13be11410b88e9b17a99c1f39eb89b3d
Ibrahim Salama <Ibrahim.salama@domims.com>
2026-09-07 15:20:58 +0300

    Update mlaio.py

--- iscstack/control/mlaio.py
+++ iscstack/control/mlaio.py
@@ -1,6 +1,3 @@
 class MLAIO:
-    """MLAIO™"""
-    def __init__(self):
-        self.name = "MLAIO"
-    def orchestrate(self, data: dict) -> dict:
-        return {"orchestrated": True, "layer": self.name}
+    def orchestrate(self, context):
+        return {"orchestrated": True, "context": context}
