a7b2168328a429aa185b476de909aa287eff8da7
Ibrahim Salama <Ibrahim.salama@domims.com>
2026-09-07 15:25:16 +0300

    Update __init__.py

--- iscstack/__init__.py
+++ iscstack/__init__.py
@@ -1,4 +1,9 @@
-__version__ = "1.0.1"
-from .control import MLVer, MLROI, MLAIO
-from .governance import AIGovCore, APIRegulator, MLRegulator
-__all__ = ["MLVer", "MLROI", "MLAIO", "AIGovCore", "APIRegulator", "MLRegulator"]
+from .control.mlver import MLVer
+from .control.mlroi import MLROI
+from .control.mlaio import MLAIO
+from .core.aigov import AIGovCore
+from .regulator.api import APIRegulator
+from .regulator.ml import MLRegulator
+
+__version__ = "1.0.2"
+__all__ = ["APIRegulator", "MLRegulator", "MLVer", "MLROI", "MLAIO", "AIGovCore"]
